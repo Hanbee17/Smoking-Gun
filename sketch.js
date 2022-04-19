7d3243e571ab7bbdb7ff26172d998caa009a434a
Hanbee17 <hanbee.cho@sjsu.edu>
2022-04-18 17:23:37 -0700

    Made the button work

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
 var canvas;
 var click1;
 var click2;
+var click3;
+var click4;
+// var click5;
+// var click6;
 var clickImg;
 
 let gameState = 'first';
@@ -13,49 +17,98 @@ function setup() {
   createCanvas(800, 800);
   frameRate(60);
   background(0);
+  InitialButton();
   titleScreen();
 }
 
 function draw() {
-  //click2.draw();
-  click1.draw();
   switch (gameState) {
     case 'title':
       titleScreen();
       break;
     case 'PopUp':
       PopUpScreen();
-      click2.draw();
       break;
     case 'main':
       mainScreen();
+      break;
+    case 'clue1':
+      cluePopUp1();
+      break;
+    case 'clue2':
+      cluePopUp2();
   }
+  // click6.draw();
+  // click5.draw();
+  click4.draw();
+  click3.draw();
+  click2.draw();
+  click1.draw();
 }
 
 function startButton() {
-  click1 = new Clickable();
-  click1.image = clickImg;
-  click1.locate(280, 500);
-  click1.resize(200, 100);
-  click1.text = "";
-    click1.onRelease = function () {
-      gameState = 'PopUp'
-    }
 }
 
 function popUpButton() {
-  click2 = new Clickable();
-  click2.text = "Click Me!";
-  click2.cornerRadius = 0;
-  click2.textScaled = true;
-  //click2.textAlign = CENTER;
-  click2.locate(400, 550);
-  click2.resize(150, 50);
-    click2.onRelease = function () {
-      gameState = 'main'
-    }
+    click1.locate(-400,-500);
+    click2.locate(325, 530);
 }
 
+function mainButton() {
+    click2.locate(-400,-500);
+    click3.locate(200,200);
+    click4.locate(600,200);
+}
+
+function InitialButton() {
+    click1 = new Clickable();
+    //click1.image = clickImg;
+    click1.locate(300, 500);
+    click1.resize(200, 100);
+    click1.color = "#FFFFFF";
+    click1.cornerRadius = 0;
+    click1.text = "Start";
+    click1.textScaled = true;
+      click1.onRelease = function () {
+        gameState = 'PopUp'
+      }
+
+    click2 = new Clickable();
+    //click2.image = clickImg;
+    click2.locate(-400, -550);
+    click2.resize(150,50);
+    //click2.color = "#FF20FF";
+    click2.cornerRadius = 0;
+    click2.text = "Got It";
+    click2.textScaled = true;
+      click2.onRelease = function () {
+        gameState = 'main'
+      }
+
+    click3 = new Clickable();
+    click3.image = clickImg;
+    click3.locate(-400, -500);
+    click3.resize(150,50);
+    click3.text = "  ";
+      click3.onRelease = function () {
+        gameState = 'clue1'
+        click4.locate(-400,-500);
+        click3.locate(-400,-500);
+      }
+
+    click4 = new Clickable();
+    click4.image = clickImg;
+    click4.locate(-400, -500);
+    click4.resize(150,50);
+    click4.text = "   ";
+      click4.onPress = function () {
+        gameState = 'clue2'
+        click4.locate(-400,-500);
+        click3.locate(-400,-500);
+      }
+}
+
+//This is the very first screen the user will see.
 function titleScreen () {
   background(100, 100, 240);
   textSize(75);
@@ -66,12 +119,13 @@ function titleScreen () {
   startButton();
 }
 
+//This is Trigger Warning Pop Up
 function PopUpScreen() {
-  rectMode(CENTER);
-  rect(400,400, 500,500);
-  fill(0);
-  textAlign(CENTER);
   push();
+  rectMode(CENTER);
+  fill(0);
+  rect(400,400, 500,500);
+  textAlign(CENTER);
   textSize(45);
   fill(214,40,40);
   text('TRIGGER WARNING', width*0.5, height*0.35);
@@ -84,72 +138,37 @@ function PopUpScreen() {
   popUpButton();
 }
 
+//This is the main page where a user can play the game.
 function mainScreen() {
   background(200,30,30);
   textAlign(CENTER);
   textSize(25);
   text('Hey! This will be the main page!', width*0.5, height*0.4);
+  mainButton();
 }
 
-// function click1() {
-//   click1 = new Clickable();
-//   click1.locate(20, 20);
-//   //This function is ran when the clickable is NOT hovered.
-//   click1.onOutside = function () {
-//     this.color = "#EEEEEE";
-//     this.text = "Move Me!"
-//     this.textColor = "#000000";
-//   }
-//   //This function is ran when the clickable is pressed.
-//   click1.onPress = function () {
-//     this.stroke = "#FF0000";
-//   }
-//   //This funcion is ran when the cursor was pressed and then
-//   //rleased inside the clickable. If it was pressed inside and
-//   //then released outside this won't work.
-//   click1.onRelease = function () {
-//     this.x += 50;
-//   }
-//
-// }
-//
-// function click2() {
-//   click2 = new Clickable();
-//   click2.cornerRadius = 0;
-//   click2.textScaled = true;
-//   click2.text = "Click Me!";
-//   click2.locate(60, 60);
-//   click2.resize(250, 100);
-//   click2.onOutside = function () {
-//     this.color = "#5E3023";
-//   }
-//   click2.onPress = function () {
-//     alert("Hello, Stranger! This is Hanbee Cho.");
-//   }
-// }
+function cluePopUp1() {
+  push();
+  rectMode(CENTER);
+  fill(0);
+  rect(400,400, 500,500,20);
+  textAlign(CENTER);
+  textSize(25);
+  fill(255);
+  text('Hey, You found the Clue 1. Congrats!!', width*0.5, height*0.35);
+  pop();
+  popUpButton();
+}
 
-  // image will stretch to fill button by default
-  // click4 = new Clickable();
-  // click4.image = clickImg;
-  // click4.imageScale = 1;
-  // click4.text = "";
-  // click4.locate(10, 200);
-  // click4.resize(120, 90);
-  // click4.onPress = function () {
-  //   click4.imageScale = 1.5
-  // }
-
-  // centered and fitted
-//   click5 = new Clickable();
-//   click5.image = clickImg;
-//   click5.fitImage = true; // no stretching!
-//   click5.imageScale = 1;
-//   click5.text = "";
-//   click5.locate(140, 200);
-//   click5.resize(120, 90);
-//   click5.onHover = function () {
-//     click5.imageScale = 1.1;
-//   }
-//   click5.onOutside = function () {
-//     click5.imageScale = 1;
-//   }
+function cluePopUp2() {
+  push();
+  rectMode(CENTER);
+  fill(0);
+  rect(400,400, 500,500,20);
+  textAlign(CENTER);
+  textSize(25);
+  fill(255);
+  text('Hey, You found the Clue 2. Congrats!!', width*0.5, height*0.35);
+  pop();
+  popUpButton();
+}
